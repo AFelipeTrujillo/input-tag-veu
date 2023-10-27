c98cc82dd0bf2ab9b26d201a8ca747b3e483ccc1
Andrés Felipe Trujillo <andres.trujillo.1@gmail.com>
2023-10-27 23:09:30 +0200

    1.0

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script>
+import inputTag from "./components/inputTag.vue"
+
+export default {
+  components : {
+    inputTag,
+  },
+  methods : {
+    handleOnTagsChange(tags) {
+      console.log(tags);
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="container">
+    <inputTag :onTagsChange="handleOnTagsChange" />
+  </div>
+</template>
+
+<style scoped>
+  body{
+    background-color: #51e5b8;
+  }
+
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 300px;
+  }
+</style>
